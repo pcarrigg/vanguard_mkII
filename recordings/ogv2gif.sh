b805a126a7b5a269735200f5fde6d8a50c40975c
wus <swunderlin@gmail.com>
2014-03-30 16:22:11 +0200

    added wing cutout comarison

---
+++ recordings/ogv2gif.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# input checks, arg must be an ogv file-name
+if [[ -z $1 || ! -f $1 ]]; then
+	echo "Usage: $0 <ogv file>";
+	exit 1;
+fi
+
+DIRS="jpg"
+base=`basename $1 .ogv`
+
+# make sure some temp dirs are available and empty
+for d in $DIRS; do
+	mkdir -p ./tmp/$d
+	rm -r ./tmp/$d/* 2>/dev/null
+done
+rm -r ./tmp/* 2>/dev/null
+
+mplayer -ao null $1 -vo jpeg:outdir=./tmp/jpg
+echo "converting jpegs to animated gif ..."
+convert ./tmp/jpg/* ./tmp/$base.gif
+echo "optimizing gif ..."
+convert ./tmp/$base.gif -fuzz 20% -layers OptimizePlus $base.gif
+
+
